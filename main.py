10351880e0c27bd0fa24cc19159d85b0e93ff689
SNA <snagithub@gmail.com>
2024-07-29 16:40:02 +0800

    repair rfid reading

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def fetch_bottles_data():
 async def rfid() -> dict:
     rfid_reader = RFIDReader()
     result = rfid_reader.start_reading()
-    print("validate: ", result)
+    print(result)
     return result
 
 
