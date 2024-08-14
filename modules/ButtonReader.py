874138778d9b73a60eb3fae891691f038afb2c1a
Sting <maks.kotov38@gmail.com>
2024-08-14 13:57:33 +0800

    new

--- modules/ButtonReader.py
+++ modules/ButtonReader.py
@@ -6,32 +6,28 @@ from modules.LedsPin import ButtonLightController  # Импортируем но
 class ButtonReader:
     """Класс для чтения состояния кнопок и управления процессом"""
 
-    def __init__(self, leds: list, slot_number: int, button_light_controller: ButtonLightController):
+    def __init__(self, slot_number: int, button_light_controller: ButtonLightController):
         self.storage = Storage()
         self.__run = True
 
-        # Ищем соответствующий Pin по номеру слота
-        pin = next((led for led in leds if led.pin_number == slot_number), None)
-        if pin is None:
-            raise ValueError(f"No LED found for slot number {slot_number}")
-
-        # Получаем адрес и пин кнопки
+        # Получаем адреса и пины для светодиода и кнопки
+        led_address, led_pin = self.storage.led_pin(slot_number)
         button_address, button_pin = self.storage.button_pin(slot_number)
 
-        # Настраиваем режимы пинов
-        pin.set_mode(PinMode.INPUT)
+        # Инициализируем пины
+        self.led_pin = Pin(led_address, led_pin)
         self.button_pin = Pin(button_address, button_pin)
-        self.button_pin.set_mode(PinMode.INPUT)
 
-        # Управляем подсветкой с помощью ButtonLightController
+        # Используем ButtonLightController для управления подсветкой
         self.button_light_controller = button_light_controller
         self.button_light_controller.turn_on_led(slot_number)  # Включаем подсветку
+
+        # Устанавливаем режимы пинов
+        self.led_pin.set_mode(PinMode.OUTPUT)
+        self.button_pin.set_mode(PinMode.INPUT)
 
         # Ожидаем нажатия кнопки
         self.button_pin.wait_for_press()
 
         # После нажатия кнопки выключаем подсветку
         self.button_light_controller.turn_off_led(slot_number)
-
-        # Настраиваем пин обратно на вывод
-        pin.set_mode(PinMode.OUTPUT)
