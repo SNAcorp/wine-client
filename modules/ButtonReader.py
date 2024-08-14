f23ec9712652fada7a35cfa45945dba138cf3b2e
Sting <maks.kotov38@gmail.com>
2024-08-14 16:38:30 +0800

    new

--- modules/ButtonReader.py
+++ modules/ButtonReader.py
@@ -1,5 +1,6 @@
 from storage.Storage import Storage
 from modules.Pin import Pin, PinMode
+from modules.LedPin import LedPin
 from modules.DrinkDispenser import DrinkDispenser
 
 class ButtonReader:
@@ -12,8 +13,8 @@ class ButtonReader:
         led_address, led_pin = self.storage.led_pin(slot_number)
         button_address, button_pin = self.storage.button_pin(slot_number)
 
-        self.led_pin = Pin(led_address, led_pin)
-        self.led_pin.set_mode(PinMode.OUTPUT)
+        self.led_pin = LedPin(led_address, led_pin)
+        self.led_pin.pin.set_mode(PinMode.OUTPUT)
         self.led_pin.write(0xFF)
 
         self.button_pin = Pin(button_address, button_pin)
--- modules/LedPin.py
+++ modules/LedPin.py
@@ -1,39 +1,36 @@
 import smbus
-from storage.Storage import Storage
+import time
 from modules.Pin import Pin, PinMode
 
 
-class ButtonLightController:
-    """Класс для управления подсветкой кнопок с использованием smbus"""
+class LedPin:
+    bus_number = 1
 
-    def __init__(self, i2c_bus: int, leds: list):
-        self.bus = smbus.SMBus(i2c_bus)  # инициализация I2C-шины
-        self.storage = Storage()
-        self.leds = leds
+    def __init__(self, address, pin_number):
+        print(f"Инициализация Pin: {self.bus_number}, 0x{address:X}, {pin_number}")
+        self.bus = smbus.SMBus(self.bus_number)
+        self.address = address
+        self.pin_number = pin_number
+        self.state = 0x00  # Initial state with all pins high (assuming active low)
+        self._write_state_led(self.state)
+        self.pin = Pin(self.address, self.pin_number)
 
-    def turn_on_led(self, slot_number: int):
-        """Включает подсветку для кнопки с указанным слотом"""
-        pin = next((led for led in self.leds if led.pin_number == slot_number), None)
-        if pin is None:
-            raise ValueError(f"No LED found for slot number {slot_number}")
 
-        button_address, _ = self.storage.button_pin(slot_number)
-        pin.set_mode(PinMode.OUTPUT)
-        self.bus.write_byte(button_address, 0xFF)  # Включаем подсветку (предполагая, что 0xFF включает светодиод)
+    def _write_state_led(self, state):
+        print(f"Запись состояния 0x{state:X} в адрес 0x{self.address:X}")
+        self.bus.write_byte(self.address, state)
 
-    def turn_off_led(self, slot_number: int):
-        """Выключает подсветку для кнопки с указанным слотом"""
-        pin = next((led for led in self.leds if led.pin_number == slot_number), None)
-        if pin is None:
-            raise ValueError(f"No LED found for slot number {slot_number}")
+    def _read_state(self):
+        state = self.bus.read_byte(self.address)
+        print(f"Чтение состояния 0x{state:X} с адреса 0x{self.address:X}")
+        return state
 
-        button_address, _ = self.storage.button_pin(slot_number)
-        pin.set_mode(PinMode.OUTPUT)
-        self.bus.write_byte(button_address, 0x00)  # Выключаем подсветку (предполагая, что 0x00 выключает светодиод)
-
-    def turn_off_all_leds(self):
-        """Выключает подсветку для всех кнопок"""
-        for pin in self.leds:
-            button_address, _ = self.storage.button_pin(pin.pin_number)
-            pin.set_mode(PinMode.OUTPUT)
-            self.bus.write_byte(button_address, 0x00)  # Выключаем подсветку для каждого светодиода
+    def write(self, value):
+        if self.pin.mode != PinMode.OUTPUT:
+            raise ValueError("Cannot write to pin not set as output")
+        print(f"Запись значения {value} на пин {self.pin_number}")
+        if value:
+            self.state |= (1 << self.pin_number)
+        else:
+            self.state &= ~(1 << self.pin_number)
+        self._write_state_led(self.state)
