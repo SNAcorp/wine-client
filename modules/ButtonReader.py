bfa740170d5b2ca83eb964982217eab063fa5454
Sting <maks.kotov38@gmail.com>
2024-08-14 18:39:01 +0800

    new

--- modules/ButtonReader.py
+++ modules/ButtonReader.py
@@ -15,9 +15,9 @@ class ButtonReader:
 
         self.led_pin = LedPumpPin(led_address, led_pin)
         self.led_pin.pin.set_mode(PinMode.OUTPUT)
-        self.led_pin.write(0xFF)
+        self.led_pin.write(0)
 
         self.button_pin = Pin(button_address, button_pin)
         self.button_pin.set_mode(PinMode.INPUT)
         self.button_pin.wait_for_press()
-        self.led_pin.write(0x00)
+        self.led_pin.write(1)
