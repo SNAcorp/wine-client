6bedaf9bb54c0b1f1e81b206f47bfe1944ed2f73
Sting <maks.kotov38@gmail.com>
2024-08-14 18:01:10 +0800

    new

--- modules/DrinkDispenser.py
+++ modules/DrinkDispenser.py
@@ -1,5 +1,5 @@
 from storage.Storage import Storage
-from modules.Pin import PinMode
+from modules.Pin import Pin, PinMode
 from modules.LedPumpPin import LedPumpPin
 import time
 
@@ -11,10 +11,10 @@ class DrinkDispenser:
         self.storage = Storage()
         print("жижа")
         pump_address, pump_pin_number = self.storage.dispander_pin(slot_number)
-        self.pump_pin = LedPumpPin(pump_address, pump_pin_number)
-        self.pump_pin.pin.set_mode(PinMode.OUTPUT)
-        self.pump_pin.write(0xFF)
+        self.pump_pin = Pin(pump_address, pump_pin_number)
+        self.pump_pin.set_mode(PinMode.INPUT)
+        self.pump_pin.write(0x00)
         print("Куралес")
         time.sleep(volume)
-        self.pump_pin.write(0x00)
+        self.pump_pin.write(0xFF)
         print("знающий")
