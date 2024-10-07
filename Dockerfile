b0a76fb691bd8eb331922151da7899bb53610fed
SNA <snagithub@gmail.com>
2024-10-07 17:54:55 +0800

    add norm functions

--- Dockerfile
+++ Dockerfile
@@ -1,73 +1,44 @@
-FROM balenalib/raspberrypi3-python:3.9-bullseye
+# Используем базовый образ Python
+FROM python:3.12-slim
 
-# Установка основных системных зависимостей
+# Устанавливаем зависимости
 RUN apt-get update && apt-get install -y \
     build-essential \
     libpq-dev \
     gcc \
-    uvicorn \
     curl \
     gnupg \
     lsb-release \
     libgtk-3-dev \
-    libsdl2-dev \
-    libsdl2-image-dev \
-    libsdl2-mixer-dev \
-    libsdl2-ttf-dev \
-    libportmidi-dev \
-    libswscale-dev \
-    libavformat-dev \
-    libavcodec-dev \
-    zlib1g-dev \
-    xclip \
-    xsel \
-    git \
-    ffmpeg \
-    libsdl2-ttf-2.0-0 \
-    pkg-config \
-    libgl1-mesa-dev \
-    libgles2-mesa-dev \
-    mesa-common-dev \
-    python3-dev \
     libwebkit2gtk-4.0-dev \
     python3-rpi.gpio \
     python3-smbus \
-    python3-spidev \
     i2c-tools \
+    chromium \
     x11-apps \
-    libmtdev-dev \
-    libgl1-mesa-glx \
-    libgles2-mesa \
-    libssl-dev \
-    libffi-dev \
+    && apt-get clean \
     && rm -rf /var/lib/apt/lists/*
 
-# Установка pip
-RUN curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && \
-    python get-pip.py && \
-    rm get-pip.py
+# Копируем файл с зависимостями
+COPY requirements.txt .
 
-# Обновление pip и установка основных Python-зависимостей
-RUN pip install --upgrade pip setuptools wheel
+# Добавление скрипта для повторных попыток установки зависимостей
+RUN echo '#!/bin/sh\n\
+n=0\n\
+until [ "$n" -ge 5 ]\n\
+do\n\
+   pip install --no-cache-dir -r requirements.txt && break\n\
+   n=$((n+1))\n\
+   sleep 5\n\
+done' > install_requirements.sh \
+    && chmod +x install_requirements.sh \
+    && ./install_requirements.sh
 
-# Установка зависимостей Kivy
-RUN pip install cython kivy kivymd
+# Копируем остальные файлы проекта
+COPY . .
+COPY wait-for-it.sh /wait-for-it.sh
+RUN chmod +x /wait-for-it.sh
 
-# Установка дополнительных зависимостей
-RUN pip install spidev RPi.GPIO smbus requests Pillow
-
-# Установка pygame
-RUN pip install pygame
-
-# Копирование приложения в контейнер
-WORKDIR /app
-COPY . /app
-
-# Установка зависимостей приложения
-RUN pip install --no-cache-dir -r requirements.txt
-
-# Установка прав на выполнение скрипта
-RUN chmod +x main.py
-
-# Команда запуска приложения
-CMD ["python", "main.py"]
+# Указываем команду для запуска приложения
+CMD ["/bin/bash", "-c", "uvicorn main:app --host 0.0.0.0 --port 80"]
+#& sleep 5 && chromium --no-sandbox --kiosk http://localhost:8000
--- storage/Storage.py
+++ storage/Storage.py
@@ -4,17 +4,10 @@ class Storage:
     """В данный момент I2C под номером 0x22 не существует,
      в дальнейшем планируется использовать её для кнопок
      и подсветки (сборки из четырёх бутылок)"""
-        
-        
-    """ Пины подсветки кнопок """
-    __button_Pin_led = {0: (0x20, 4), 1: (0x20, 5), 2: (0x20, 6), 3: (0x20, 7),
-                        4: (0x22, 4), 5: (0x22, 5), 6: (0x22, 6), 7: (0x22, 7)}
 
     __led_bottle = {0: (0x23, 0), 1: (0x23, 1), 2: (0x23, 2), 3: (0x23, 3),
                     4: (0x23, 4), 5: (0x22, 3), 6: (0x23, 6), 7: (0x23, 7)}
-    """ Пины для взимодействия с сигналом кнопок """
-    __button_Pin = {0: (0x20, 0), 1: (0x20, 1), 2: (0x20, 2), 3: (0x20, 3),
-                    4: (0x22, 0), 5: (0x22, 1), 6: (0x22, 2), 7: (0x22, 3)}
+
 
     """ Пины для управления насосами (диспенсером) """
     __pump_Pin = {0: (0x21, 0), 1: (0x21, 1), 2: (0x21, 2), 3: (0x21, 3),
@@ -23,24 +16,9 @@ class Storage:
     # Программа не учитывает нажатие на кнопку.
     # Настроить выборку значения для wait_for_button_press
     # доработать отчищение пинов и вообщем всей программы после завершениия
-    
-    """ Пин для замены бутылки (Для кнопки) """
-    __button_for_replacement = 36 #Настроить нормальные пины
-
-    """ Пин для замены бутылки (Для подсветки кнопки) """
-    __button_led_for_replacement = 34 #Настроить нормальные пины
-
-    """ Результат работы программы """
-    result_of_program = False
-
-    """!!!Реализовать подгрузку имен вин с файла!!!"""
-    __names_of_vine = {1: "1 Vine", 2: "2 Vine", 3: "3 Vine", 4: "4 Vine"}
 
     """Ссылка на сервер"""
     __server_url = "http://localhost"
-
-    """Ссылка на сервер для замены бутылки в аппарате"""
-    __server_url_for_replace_bottle= ""
 
     """Путь к файлу с логами основного цикла программы"""
     __path_to_log_file = "technical_information/log.json"
@@ -138,51 +116,7 @@ class Storage:
         """
         return self.__pump_Pin.get(number_of_bottle)
 
-    def led_pin(self, number_of_button: int):
-        """
-        ID нужного нам пина подстветки по номеру кнопки (по раскладке BOARD)
-        """
-        return self.__button_Pin_led.get(number_of_button)
 
-    def button_pin(self, number_of_button: int):
-        """
-        ID пина по номеру кнопки
-        """
-        return self.__button_Pin.get(number_of_button)
-
-    @property
-    def button_for_replacement(self):
-        """
-        ID пина кнопки для замены бутылки
-        """
-        return self.__button_for_replacement
-
-    @property
-    def button_led_for_replacement(self):
-        """
-        ID пина подсветки кнопки для замены бутылки
-        """
-        return self.__button_led_for_replacement
-
-    @property
-    def result(self) -> bool:
-        """
-        Резултат работы одного цикла программы
-        """
-        return self.result_of_program
-
-    @result.setter
-    def result(self, value: bool):
-        """
-        Изменение промежуточного результата
-        """
-        self.result_of_program = value
-
-    def get_vine_name(self, bottle_number) -> str:
-        """
-        Получение наименования вина для формирования модели чека
-        """
-        return self.__names_of_vine.get(bottle_number)
 
     @property
     def server_url(self) -> str:
@@ -198,16 +132,4 @@ class Storage:
         """
         return self.__link_for_rfid
 
-    @property
-    def server_url_for_replace_bottle (self) -> str:
-        """
-        Получение ссылку для замены бутылки в аппарате
-        """
-        return self.__server_url_for_replace_bottle
 
-    @server_url_for_replace_bottle.setter
-    def server_url_for_replace_bottle(self, value):
-        """
-        Формируем ссылку для конкретного аппарата
-        """
-        self.__server_url_for_replace_bottle = ""
