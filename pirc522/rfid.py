d91f6fdaed37cc03ce0576e22f6ac0dda298915c
SNA <snagithub@gmail.com>
2024-10-09 17:09:49 +0800

    add norm functions

--- pirc522/rfid.py
+++ pirc522/rfid.py
@@ -10,12 +10,16 @@ board = RASPBERRY
 def_pin_rst = 22
 def_pin_irq = 18
 # Try with Raspberry PI imports first
-import spidev
-import RPi.GPIO as GPIO
-SPIClass = spidev.SpiDev
-def_pin_rst = 22
-def_pin_irq = 18
-def_pin_mode = GPIO.BOARD
+try:
+    # Try with Raspberry PI imports first
+    import spidev
+    import RPi.GPIO as GPIO
+    SPIClass = spidev.SpiDev
+    def_pin_rst = 22
+    def_pin_irq = 18
+    def_pin_mode = GPIO.BOARD
+except ImportError:
+    print("Failed to import spidev and/or RPi.GPIO")
 
 
 class RFID(object):
